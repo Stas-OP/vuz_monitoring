57ec4e895754dbc3e4b476da3f71ba0c1814738c
stas <63487852+Stas-OP@users.noreply.github.com>
2024-11-10 13:16:49 +0300

    Delete main — копия directory

--- main — копия/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ComplaintStatus, Building, Department, Complaint
-
-# Register your models here.
-admin.site.register(ComplaintStatus)
-admin.site.register(Building)
-admin.site.register(Department)
-admin.site.register(Complaint)
